f61a7703f4751e79d647684738c8cc7e9e8e319b
shengx <shengx@ickey.cn>
2023-05-04 00:16:40 +0800

    update py

--- checkin.py
+++ checkin.py
@@ -18,17 +18,15 @@ def start():
     referer = 'https://glados.rocks/console/checkin'
     checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer }, data = json.dumps({"token": "glados.network"}))
     state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
-    print(checkin.json())
+    # print(checkin.json())
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
-        #print(time)
-        if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+        print(time+  ' '+ 'checkin success' )
     else:
-        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+        print(time+  ' '+ 'checkin failed' )
 
 def main_handler(event, context):
   return start()
